eb4425064c66fc57084c1e11d8c25ea438415077
Md Rafiqul islam <rafiqul.senior.istvn@gmail.com>
2020-01-09 17:02:30 +0700

    first commit

---
+++ crud/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String LastName { get; set; }
+        public String FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+
+    }
+}
